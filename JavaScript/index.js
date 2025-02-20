f8c93c79c6f59b8de6e26658a6fc8eb4b6a4e6d5
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-02-20 13:33:33 +0530

    Practice Code of JacaScript

---
+++ JavaScript/index.js
@@ -0,0 +1,120 @@
+// let number=7
+// console.log(number)
+// console.log("Hellow world")
+
+
+
+
+// let silent={
+//     name: "Silent Killer",
+//     age: 20,
+//     standard: 12,
+// }
+// console.log(silent)
+
+
+// let num=10
+// if (num>10) {
+//     console.log('Number is greater than 10')
+// }
+// else{
+//     console.log("Number is less than 10")
+// }
+
+
+
+
+// let strings=90;
+
+// let convertString=strings.toString();
+
+// console.log(typeof convertString);
+
+
+
+// console.log(typeof String(strings));
+
+/* Grade Calculator Problem 
+let grade="30%";
+let number= +grade;
+if (number>= Number("90%")){
+    console.log("Grade: A")
+}
+else if(number>= Number("80%")){
+    console.log("Grade: B")
+}
+else if(number >= Number("60%")){
+    console.log("Grade: C")
+}
+else if(number >= Number("50%")){
+    console.log("Grade: D")
+}
+else{
+    console.log("Fail")
+}
+
+
+console.log(typeof Number("90%"));
+ */
+/* Write a program that checks if a number is even or odd using an if-else statement.
+let num=4;
+if (num % 2 === 0){
+    console.log("Even Number")
+}
+else{
+    console.log("Odd number")
+}
+*/
+
+
+/*Write a program that takes a number and checks if it is positive, negative, or zero.
+
+let Numbers= 2;
+
+if(Numbers>0){
+    console.log("Positive Nmber")
+}
+else{
+    console.log("Negative Number")
+}
+*/
+/*Write a program that takes two numbers as input and prints the larger one.
+
+let num1 = null;
+let num2 = null;
+
+
+if (num1 > num2) {
+    console.log("Greter Number");
+
+}
+else if (num1 < num2) {
+    console.log("Smaller Number");
+    
+}
+else if(num1=num2){
+    console.log("Equal");
+
+}
+else {
+    console.log("enter your number");
+}*/
+
+/*Write a program that checks if a person is eligible to vote (age must be 18 or above).
+
+let age=17;
+
+if(age >= 18){
+    console.log("Eligible for voting")
+}
+else{
+    console.log("Bap ko bol ja ke vote kare")
+}*/
+
+let a = 35;
+if (a % 5 == 0 && a % 7 == 0) {
+    console.log("It is divisible by both 5 and 7")
+}
+else {
+    console.log("Not divisible by both 5 and 7")
+}
