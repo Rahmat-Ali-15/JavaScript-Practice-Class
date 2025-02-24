e25950b69f5ea06f5bbcf4671bbffe3ec3fb0ef2
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-02-24 12:48:35 +0530

    Switch Case Practice

---
+++ JavaScript/Switch/index.js
@@ -0,0 +1,25 @@
+// Switch
+
+
+let num1=5;
+let num2=3;
+
+let flag=true;
+
+switch(flag){
+    case num1>num2:
+        console.log(num1, "is greater num.")
+        break
+
+    case num1<num2:
+        console.log(num2, "is greater num.")
+        break
+    case num1=num2:
+    console.log("equal")
+    break
+
+    default: console.log(num1,num2)
+
+}
+
+// -> --> ===  
