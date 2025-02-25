717f06583ac74ecadba908cb25dc45bee04f6565
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-02-25 10:04:28 +0530

    It is a program that takes a number from 1 to 7 and prints the corresponding day of the week.

---
+++ JavaScript/Days of the Week(By Using Switch)/index.js
@@ -0,0 +1,30 @@
+// Days of the Week
+// Write a program that takes a number from 1 to 7 and prints the corresponding day of the week.
+
+let day = k;
+
+switch (day){
+    case 1:
+        console.log("Sunday")
+        break
+    case 2:
+        console.log("Monday")
+        break
+    case 3:
+        console.log("Tuesday")
+        break
+    case 4:
+        console.log("Wednesday")
+        break
+    case 5:
+        console.log("Thursday")
+        break
+    case 6:
+        console.log("Friday")
+        break
+    case 7:
+        console.log("Saturday")
+        break
+    default:
+        console.log("Enter a valid number")
+}
