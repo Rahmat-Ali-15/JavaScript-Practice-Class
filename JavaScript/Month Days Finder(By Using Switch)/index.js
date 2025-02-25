c48fc11eb457d38f089b610135a9995bdd3b060e
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-02-25 12:27:43 +0530

    It is a program that takes a month number (1-12) and prints name and numbers of days in that month

---
+++ JavaScript/Month Days Finder(By Using Switch)/index.js
@@ -0,0 +1,46 @@
+// Month Days Finder
+// Write a program that takes a month number (1-12) and prints the number of days in that month.
+
+let monthNumber = 11;
+
+switch (monthNumber){
+    case 1:
+        console.log("January 31 Days");
+        break
+    case 2:
+        console.log("Febuary 28 or 29 Days (Leap year)");
+        break
+    case 3:
+        console.log("March 31 Days");
+        break
+    case 4:
+        console.log("April 30 Days");
+        break
+    case 5:
+        console.log("May 31 Days");
+        break
+    case 6:
+        console.log("June 30 Days");
+        break
+    case 7:
+        console.log("July 31 Days");
+        break
+    case 8:
+        console.log("August 30 Days");
+        break
+    case 9:
+        console.log("September");
+        break
+    case 10:
+        console.log("October 31 Days");
+        break
+    case 11:
+        console.log("November 30 Days");
+        break
+        case 12:
+            console.log("December 31 Days");
+            break
+    default:
+        console.log("Enter a number between")
+}
+            
