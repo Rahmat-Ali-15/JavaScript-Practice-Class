e36ba1842700a6190970ee080ebbc8ff5d81fc90
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-02-27 01:30:05 +0530

    It is a program that checks whether a given number is even or odd by using switch-case.

--- JavaScript/Even or Odd(By Using Switch Case)/index.js
+++ JavaScript/Even or Odd(By Using Switch Case)/index.js
@@ -1,4 +1,20 @@
 // Even or Odd
 // Write a switch-case program that checks whether a given number is even or odd. Use the modulo operator (%).
 
-let 
+let num1 = 0;
+switch (true){
+    case num1 === 0:
+        console.log("Number cannnot be zero")
+        break
+    case num1 % 2 === 0:
+        console.log("Even Number")
+        break
+    case num1 % 2 === 1:
+        console.log("Odd Number")
+        break
+    case num1 % 2 === -1:
+        console.log("Odd Number")
+        break
+    default: 
+        console.log("Invalid")
+}
