e5c65e0b5bceaee4ea990e29870e59a13494b7d8
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-02-28 12:50:32 +0530

    Js code for finding sum of first 10 natural number

---
+++ JavaScript/For loop Questions/Sum of 1st 10 Natural Number/index.js
@@ -0,0 +1,12 @@
+// Print the sum of the first 10 natural numbers using a for loop.
+
+
+let array= [1,2,3,4,5,6,7,8,9,10];
+let count= 0;
+
+for (let i = 0; i< array.length; i++){
+    count += array[i]
+    
+}
+console.log(count)
+console.log(array.length)
