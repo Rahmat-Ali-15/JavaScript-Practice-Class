9a6e2c9e63d91bb19371c8a5fe328e691656dd2f
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-02-28 12:28:40 +0530

    Js code for finding even and odd number

---
+++ JavaScript/For loop Questions/Print Even number between 1 & 100/index.js
@@ -0,0 +1,11 @@
+// Prints all even numbers between 1 and 100 using a for loop.
+
+
+for(let i= 1; i<=100; i++){
+    if(i % 2 === 0){
+        console.log(i,"Even Number")
+    }
+    else{
+        console.log(i,"Odd number")
+    }
+}
