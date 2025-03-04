917d81071301f7181675dd1b8eab7c9bf0766d1f
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-03-04 09:34:20 +0530

    Practice questions on js

---
+++ JavaScript/For loop Questions/Print Hello, World! 5 times using a for loop/index.js
@@ -0,0 +1,50 @@
+// Print Hello, World! 5 times using a for loop.
+
+// for ( let i= 1; i<=10; i++){
+//     console.log("Hellow, World!")
+// }
+
+
+
+// Use a for loop to display numbers from 1 to 100 but only multiples of 10.
+
+// for(let i =1; i<=10; i++){
+//     console.log(i*10)
+// }
+
+// Write a program to find the factorial of a number using a for loop.
+
+// let factNum = 10;
+// let factStart = 1;
+// for (i = 1; i <= factNum; i++) {
+//     factStart *= i
+// }
+// console.log(factStart)
+
+
+
+// Print all the letters of the word "PROGRAMMING" using a for loop.
+
+// let word = "PROGRAMMING";
+
+// for (i = 0; i < word.length; i++) {
+//     console.log(word[i])
+// }
+
+
+// Use a for loop to reverse a string.
+
+// for (let i=8;i>0;i--){
+//     console.log(i)
+// }
+
+
+// Write a program to find the sum of digits of a number using a for loop.
+
+// let num = 15;
+// let sum = 15;
+
+// for (let i = 0; i < num; i++) {
+//     sum += num
+// }
+// console.log(sum)
