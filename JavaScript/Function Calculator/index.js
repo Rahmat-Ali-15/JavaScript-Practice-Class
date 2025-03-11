d0e8a62ec9ce1f9fb2b089a7c02cf821b28c8d09
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-03-11 05:39:18 +0530

    Updated index.js

--- JavaScript/Function Calculator/index.js
+++ JavaScript/Function Calculator/index.js
@@ -4,9 +4,9 @@ let num1 = prompt("Enter 1st Number");
 let operator = prompt("Enter Operator");
 let num2 = prompt("Enter 2nd Number");
 
-num1 = +num1[0];
-num2 = +num2[0];
-operator = operator[0];
+num1 = +num1;
+num2 = +num2;
+operator = operator;
 
 // function calculate() {
 //     switch (operator) {
@@ -40,11 +40,16 @@ function calculate(n1, n2, key) {
             return num1 * num2;
         case "/":
             return num1 / num2;
+        case "%":
+            return ((num1 * num2) / 100);
         default:
             break;
     }
 }
 
 let output = calculate(num1, num2, operator);
+let value = document.getElementById("result")
 
-console.log(output);
+value.append(output);
+
+// console.log(output);
