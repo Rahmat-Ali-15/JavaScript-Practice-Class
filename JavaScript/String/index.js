ec02141ca2892b410ba5dbb227b0b0b2eef608a0
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-03-17 02:18:48 +0530

    Practice code of string

---
+++ JavaScript/String/index.js
@@ -0,0 +1,12 @@
+// 1. Prompt the user to enter their ful lname. Generate the username for them based on the input. Start username with @, followed by their full name and eneding with the fullname length.
+
+
+// let fullName= prompt("Enter your fullname without space");
+// let username= "@" + fullName + fullName.length;
+
+// console.log(`@${fullName.toLocaleLowerCase()}${fullName.length}`)
+
+// console.log(username);
+
+
+
