427a45fb26f5634a5a51d9cb86b356c03e1ac774
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-03-20 10:03:04 +0530

    Code of methods of array and practice questions

--- JavaScript/Array/index.js
+++ JavaScript/Array/index.js
@@ -1,4 +1,4 @@
-// How do you add an element to the end of an array.
+// 1. How do you add an element to the end of an array.
 
 // let arr = [1,2,3,4,5]
 
@@ -7,4 +7,75 @@
 // console.log(arr);
 
 
-// 
+// 2. How do you remove the first element from an array?
+
+// let arr = [1,2,3,4];
+
+// arr.shift(arr[1]);
+// console.log(arr);
+
+// 3. How do you find the length of an array?
+
+// let arr = [1,2,3,4,5,6,7,8,9];
+
+// console.log(arr.length);
+
+
+// 4. How do you create a new array where each element is doubled?
+
+// let arr = [10,15,20,25,30];
+
+// let newArray = arr.map((number, index) => {
+//     return arr[index] * 2
+// })
+
+// console.log(newArray);
+
+
+
+// 5. How do you filter out all numbers greater than 50 from an array?
+
+// let arr = [20, 40, 30, 80, 90, 2, 23, 60, 70];
+
+// let newArray = arr.filter((number) => {
+//     return number > 50;
+// })
+// console.log(newArray);
+
+
+
+// 6. How do you find the sum of all elements in an array?
+
+
+// let arr = [10, 30, 50, 70];
+
+// let sum = arr.reduce((accumelator, current) => {
+//     return accumelator + current;
+// }, 0)
+// console.log(sum);
+
+
+
+// 7. How do you check if an array contains the number 10 ?
+
+
+// let arr = [10, 30, 50,60, 70];
+
+// let hasValue= arr.includes(20);
+
+// console.log(hasValue);
+
+
+
+// 8. How do you find the first element in an array that is greater than 20?
+
+
+// let arr = [10, 15, 20,19, 70];
+
+// let result = arr.find(num=> num>20);
+// console.log(result);
+
+
+// 9. How do you find the index of a specify element in an array?
+
+
