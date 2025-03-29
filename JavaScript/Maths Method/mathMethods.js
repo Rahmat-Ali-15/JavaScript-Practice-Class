c71d5e52bb2f38c92133315e32d949cd5ee758c7
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-03-29 08:11:33 +0530

    Practice code of math methods

---
+++ JavaScript/Maths Method/mathMethods.js
@@ -0,0 +1,14 @@
+// ==> To finding square root of a given number.
+
+// by using method
+let num1=49;
+let ans1= Math.sqrt(num1)
+console.log(ans1);
+
+// without method
+let num = 25;
+let ans=num**(1/2);
+console.log(ans);
+
+
+
