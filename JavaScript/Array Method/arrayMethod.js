ca42e0847b160bdac7ddfb68a8d162306b16727f
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-03-30 00:44:29 +0530

    Practice code of array methods

--- JavaScript/Array Method/arrayMethod.js
+++ JavaScript/Array Method/arrayMethod.js
@@ -1,3 +1,9 @@
+// Array.isArray() ==> It is used to check whether the passed value is an array.
+
+let arrCheck = [5,3,4];
+console.log(Array.isArray(arrCheck));
+
+
 // Array.push() ==> It is used to add elements at the end of the array. It returns new array.
 
 let arr = [5,4,2,7,9];
@@ -32,3 +38,26 @@ let arr4 = [5,8,9,7,6,5,3,70];
 let sliceArray = arr4.slice(1,5);
 console.log(sliceArray);
 
+// Array.splice() ==> It is used to add & remove elements from an array. It can remove existing element and add new element in place of that removed element. We can also add elements without removing existing elements.To use this method we give the starting index & then we write how many elements is to be romoved or add. It change the original array.
+
+let arr5 = [8,5,3,2,41,52,20,42,48,10,70,80];
+let spliceArray = arr5.splice(1,2)
+console.log(arr5);
+
+
+// Array.at() ==> It takes integer value(index) and returns the item at that index. Negative value return items from end.
+
+let arr6 = [5,3,7,10,15];
+console.log(arr6.at(3));
+console.log(arr6.at(-1));
+
+
+// Array.indexOf() ==> It is used to find index of the element in an array. It returns the first index match with that element. If there are more than one same elements is present then we can also give the starting index and then we can find the element index. If we find the element which is not present in the array then it returns index -1.
+
+let arr7 = ["banana","potato","tomato","apple","onion","potato"];
+console.log(arr7.indexOf("potato"));
+console.log(arr7.indexOf("potato",2));
+console.log(arr7.indexOf("mango"));
+
+
+// 
