b690c88e8d8d7a857b3f1c6c727d8702dd9e7449
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-04-03 08:09:43 +0530

    Practice code of promises

--- JavaScript/Promises/promises.js
+++ JavaScript/Promises/promises.js
@@ -26,12 +26,12 @@
 // this -> invoke the current object
 
 let ice_cream = new Promise((resolve, reject) => {
-    let got_ice_cream = true; // api se aa rha hai either wo true hoga ya false
+    let got_ice_cream = false; // api se aa rha hai either wo true hoga ya false
     if (got_ice_cream) {
-        resolve([(status=got_ice_cream), (payload="done")]);
+        resolve({status : got_ice_cream, payload : "done"});
     }
     else {
-        reject([(status=got_ice_cream), (payload="not done")]);
+        reject({status : got_ice_cream, payload : "not done"});
     }
 })
 
@@ -42,7 +42,7 @@ let ice_cream = new Promise((resolve, reject) => {
 // this is handling promis status
 // console.log(ice_cream);
 
-ice_cream.then((res) => { console.log("got ice cream in hand"); }).catch((res) => { console.log("ice cream is not available"); })
+ice_cream.then((res) => { console.log("got ice cream in hand"); }).catch((err) => { console.log("ice cream is not available"); })
 
 
 // Api call
@@ -51,8 +51,26 @@ ice_cream.then((res) => { console.log("got ice cream in hand"); }).catch((res) =
 
 let api = "https://jsonplaceholder.typicode.com/todos";
 
-fetch(api).then((res) => res.json()).then((res) => { console.log(res); }).catch((err) => { console.log(err); })
+// fetch(api).then((res) => res.json()).then((res) => { console.log(res); }).catch((err) => { console.log(err); })
 
 
 // redableStream mila jo lock tha.
 // unclock with prototype -> json()
+
+
+
+let post = new Promise((resolve, reject) => {
+    let post_detail = true;
+    if (post_detail) {
+        resolve(post_detail);
+    }
+    else {
+        reject(post_detail);
+    }
+})
+
+post.then((res)=>{console.log("got post details");}).catch((err)=>{console.log("post details not available");})
+
+let api1 = "https://jsonplaceholder.typicode.com/posts?_limit=100"
+
+fetch(api1).then((res) => res.json()).then((res) => { console.log(res); }).catch((err) => { console.log(err); })
