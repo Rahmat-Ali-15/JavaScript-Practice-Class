acac9df542073101fcd69f6772cfdd45e737500b
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-04-17 05:18:14 +0530

    Added type & year functionality and added default functionality

--- JavaScript/Movie App/movieApp.js
+++ JavaScript/Movie App/movieApp.js
@@ -1,16 +1,18 @@
-let movieUrl = `https://omdbapi.com/?apikey=3e431991`
+const api_key = `3e431991`
+let movieUrl = `https://omdbapi.com/?apikey=${api_key}`;
 
-async function movieApp() {
-    let search = document.getElementById("search").value;
-    console.log(search);
+async function movieApp(defaultSearch = "") {
+    let search = defaultSearch || document.getElementById("search").value;  // defaultSearch parameter is used for showing Avengers movie while loading the webpage
+    let type = document.getElementById("select_type")?.value || "";
+    let year = document.getElementById("year_input")?.value || "";
+    // console.log(search);
 
-    let api = movieUrl + `&s=${search}`
-
+    let api = movieUrl + `&s=${search}&y=${year}&type=${type}`;
+    
     try {
         let res = await fetch(api);
         let data = await res.json();
-        console.log(data.Search
-        );
+        console.log(data.Search);
         foundMovie(data.Search);
     } catch (error) {
         console.log(error);
@@ -19,6 +21,7 @@ async function movieApp() {
 
 function foundMovie(value) {
     let main = document.getElementById("container");
+    main.innerHTML = "";  // clear previous results
 
     value.forEach((el) => {
         let div = document.createElement("div");
@@ -37,9 +40,22 @@ function foundMovie(value) {
         Type.innerText = `Type: ${el.Type}`;
         Poster.src = `${el.Poster}`;
 
-        div2.append(Title, Year, imdbID, Type)
+        div2.append(Title, Year, imdbID, Type);
         div.append(div2, Poster);
-        main.append(div)
+        main.append(div);
 
     })
 }
+
+// Avengers word will show in the movie name input
+/*window.onload = function () {
+    document.getElementById("search").value = "Avengers";
+    movieApp();
+};*/ 
+
+
+// Avengers word will not show in the movie name input
+
+window.onload = function () {
+    movieApp("Avengers"); // But don't show it in the input
+};
