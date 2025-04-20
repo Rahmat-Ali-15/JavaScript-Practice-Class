7a671dfd4d7a8b6fc5d23f5de5b940f5e894c265
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-04-21 00:00:07 +0530

    Code of the day

--- JavaScript/For loop Questions/Practice Question/index.js
+++ JavaScript/For loop Questions/Practice Question/index.js
@@ -98,3 +98,10 @@ for (let i = 1; count < 10; i += 2) {
     console.log(i);
     count++
 }
+
+
+// Print the square of numbers from 1 to 10.
+
+for (let i = 1; i<= 5; i++){
+    console.log(i*i);
+}
