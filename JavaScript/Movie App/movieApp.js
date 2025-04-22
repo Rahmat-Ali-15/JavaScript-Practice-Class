39a339b188982457fe970526c666ed8ab41d9042
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-04-22 23:50:56 +0530

    Code of the day

--- JavaScript/Movie App/movieApp.js
+++ JavaScript/Movie App/movieApp.js
@@ -46,7 +46,7 @@ function foundMovie(value) {
         div.className = "parent_container"
         let div2 = document.createElement("div");
         div2.className = "p_h5_container"
-       
+
 
         let Title = document.createElement("h5");
         let Year = document.createElement("p");
@@ -61,12 +61,32 @@ function foundMovie(value) {
         Poster.src = `${el.Poster}`;
         // Poster.src = `${el.Poster}`;
         Poster.style.cursor = "pointer";
-        Poster.addEventListener("click", () => {
-            window.open(`https://omdbapi.com/?apikey=3e431991&i=${el.imdbID}`, "_blank");
-        });
+        // Poster.addEventListener("click", () => {
+        //     window.open(`https://omdbapi.com/?apikey=3e431991&i=${el.imdbID}`, "_blank");
+        // });
+
+
+        const btn = document.createElement("button")
+        btn.innerText = "click"
+        btn.id= "btn_modal"
+
+        // const btn_modal = document.getElementById("btn_modal");
+        // const modalContainer = document.getElementById("modal-container")
+        // const modal_close_btn = document.getElementById("modal_close_btn")
+
+        // btn_modal.addEventListener('click', ()=>{
+        //     modalContainer.classlist.add("show");
+        // });
+
+
 
         div2.append(Title, Year, imdbID, Type);
-        div.append(div2, Poster);
+        div.append(div2, Poster, btn);
+
+        btn.addEventListener("click", function () {
+            movieDescription(el.imdbID)
+        })
+
         main.append(div);
 
     })
@@ -74,73 +94,75 @@ function foundMovie(value) {
 
 // let descriptionAPI= `${movieUrl}&i=${imdbID}`
 
-// async function movieDescription() {
-//     let descriptionAPI= `https://omdbapi.com/?apikey=3e431991&i=${el.imdbID}`
-    
-//     try {
-//         let res = await fetch(descriptionAPI);
-//         let description = await res.json();
-//         movieDescription(description)
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
+async function movieDescription(id) {
+    // console.log("invoked");
+    let descriptionAPI = `https://omdbapi.com/?apikey=3e431991&i=${id}`
 
-// function movieDescription(val){
-//     let main2 = document.getElementById("movie_description");
-//     main2.innerHTML = "";  // clear previous results
+    try {
+        let res = await fetch(descriptionAPI);
+        let description = await res.json();
+        // console.log(description, `this is our data`);
+        movieDetails(description)
+    } catch (error) {
+        console.log(error);
+    }
+}
 
-//     val.forEach(()=>{
-//         let detalsDiv = document.createElement("div");
-
-//         let Rated = document.createElement("p");
-//         let Released = document.createElement("p");
-//         let Runtime = document.createElement("p");
-//         let Gener = document.createElement("p");
-//         let Director = document.createElement("h4");
-//         let Writer = document.createElement("p");
-//         let Actors = document.createElement("p");
-//         let Plot = document.createElement("p");
-//         let Language = document.createElement("p");
-//         let Country = document.createElement("p");
-//         let Awards = document.createElement("p");
-//         // let Posters = document.createElement("p");
-//         let Ratings = document.createElement("p");
-//         let Metascore = document.createElement("p");
-//         let imdbRating = document.createElement("p");
-//         let imdbVotes = document.createElement("p");
-//         let DVD = document.createElement("p");
-//         let Boxoffice = document.createElement("p");
-//         let Website = document.createElement("p");
-//         let Response = document.createElement("p");
-
-//         Rated.innerText = `{Rated: ${el.Rated}}`;
-//         Released.innerText = `{Released: ${el.Realeased}}`;
-//         Runtime.innerText = `{Runtime: ${el.Runtime}}`;
-//         Gener.innerText = `{Gener: ${el.Gener}}`;
-//         Director.innerText = `{Director: ${el.Director}}`;
-//         Writer.innerText = `{Writer: ${el.Writer}}`;
-//         Actors.innerText = `{Actors: ${el.Actors}}`;
-//         Plot.innerText = `{Plot: ${el.Plot}}`;
-//         Language.innerText = `{Language: ${el.Language}}`;
-//         Country.innerText = `{Country: ${el.Country}}`;
-//         Awards.innerText = `{Awards: ${el.Awards}}`;
-//         // Posters.innerText = `{Posters: ${el.Poster}}`;
-//         Ratings.innerText = `{Ratings: ${el.Ratings}}`;
-//         Metascore.innerText = `{Metascore: ${el.Metascore}}`;
-//         imdbRating.innerText = `{imdbRating: ${el.imdbRating}}`;
-//         imdbVotes.innerText = `{imdbVotes: ${el.imdbVotes}}`;
-//         DVD.innerText = `{DVD: ${el.DVD}}`;
-//         Boxoffice.innerText = `{Boxoffice: ${el.Boxoffice}}`;
-//         Website.innerText = `{Website: ${el.Website}}`;
-//         Response.innerText = `{Response: ${el.Response}}`;
+function movieDetails(val) {
+    let main2 = document.getElementById("movie_description");
+    main2.innerHTML = "";  // clear previous results
 
 
-//         detalsDiv.append(Rated,Released, Runtime, Gener, Director, Writer,Actors, Plot, Language, Country, Awards, Ratings, Metascore, imdbRating, imdbVotes, DVD, Boxoffice, Website, Response);
-//         main2.append(div)
-//     })
+    let detalsDiv = document.createElement("div");
 
-// }
+    let Rated = document.createElement("p");
+    let Released = document.createElement("p");
+    let Runtime = document.createElement("p");
+    let Gener = document.createElement("p");
+    let Director = document.createElement("h4");
+    let Writer = document.createElement("p");
+    let Actors = document.createElement("p");
+    let Plot = document.createElement("p");
+    let Language = document.createElement("p");
+    let Country = document.createElement("p");
+    let Awards = document.createElement("p");
+    // let Posters = document.createElement("p");
+    let Ratings = document.createElement("p");
+    let Metascore = document.createElement("p");
+    let imdbRating = document.createElement("p");
+    let imdbVotes = document.createElement("p");
+    let DVD = document.createElement("p");
+    let Boxoffice = document.createElement("p");
+    let Website = document.createElement("p");
+    let Response = document.createElement("p");
+
+    Rated.innerText = `Rated: ${val.Rated}`;
+    Released.innerText = `Released: ${val.Realeased}`;
+    Runtime.innerText = `Runtime: ${val.Runtime}`;
+    Gener.innerText = `Gener: ${val.Gener}`;
+    Director.innerText = `Director: ${val.Director}`;
+    Writer.innerText = `Writer: ${val.Writer}`;
+    Actors.innerText = `Actors: ${val.Actors}`;
+    Plot.innerText = `Plot: ${val.Plot}`;
+    Language.innerText = `Language: ${val.Language}`;
+    Country.innerText = `Country: ${val.Country}`;
+    Awards.innerText = `Awards: ${val.Awards}`;
+    // Posters.innerText = `Posters: ${val.Poster}`;
+    Ratings.innerText = `Ratings: ${val.Ratings}`;
+    Metascore.innerText = `Metascore: ${val.Metascore}`;
+    imdbRating.innerText = `imdbRating: ${val.imdbRating}`;
+    imdbVotes.innerText = `imdbVotes: ${val.imdbVotes}`;
+    DVD.innerText = `DVD: ${val.DVD}`;
+    Boxoffice.innerText = `Boxoffice: ${val.Boxoffice}`;
+    Website.innerText = `Website: ${val.Website}`;
+    Response.innerText = `Response: ${val.Response}`;
+
+
+    detalsDiv.append(Rated, Released, Runtime, Gener, Director, Writer, Actors, Plot, Language, Country, Awards, Ratings, Metascore, imdbRating, imdbVotes, DVD, Boxoffice, Website, Response);
+    main2.append(detalsDiv)
+
+
+}
 
 
 // Avengers word will show in the movie name input
@@ -155,3 +177,13 @@ function foundMovie(value) {
 window.onload = function () {
     movieApp("Avengers"); // But don't show it in the input
 };
+
+
+
+const btn_modal = document.getElementById("btn_modal");
+const modalContainer = document.getElementById("modal-container")
+const modal_close_btn = document.getElementById("modal_close_btn")
+
+btn_modal.addEventListener('click', ()=>{
+        modalContainer.classlist.add("show");
+    });
