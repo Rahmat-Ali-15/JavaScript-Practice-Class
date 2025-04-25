943cbf8c771bb5c837eec04cb809d23da8dede50
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-04-25 19:02:45 +0530

    Updated code

--- JavaScript/Form Validation (Submit Form)/index.js
+++ JavaScript/Form Validation (Submit Form)/index.js
@@ -6,6 +6,17 @@ function myValidation(event) {
     let User_Confirm_Password = document.getElementById("c_password").value;
     let User_Contact = document.getElementById("contact").value;
 
+    // Add error
+    let Contact_Error = document.querySelector(".contact_error");
+    let Email_Error = document.querySelector(".email_error");
+    let Password_Error = document.querySelector(".password_error")
+    let Confirm_Password_Error = document.querySelector(".confirm_password_error");
+
+    // Remove Previous Error
+        Contact_Error.textContent = "";
+        Email_Error.textContent = "";
+        Password_Error.textContent = "";
+        Confirm_Password_Error.textContent = "";
 
     let object = {
         email: User_Email,
@@ -13,43 +24,68 @@ function myValidation(event) {
         confirm_password: User_Confirm_Password,
         contact: User_Contact,
     }
-
-    // console.log(object);
     localStorage.setItem("userData", JSON.stringify(object));
 
-    if(User_Email==="" || User_Password === "" || User_Confirm_Password === "" || User_Contact === ""){
+
+    if(User_Email=== "" || User_Password === "" || User_Confirm_Password === "" || User_Contact === ""){
         alert("All fields are mendatory!")
         return false;
-    }
-    else{
-        true;
     }
 
     // Email validation
 
     if (User_Email.indexOf("@") <= 0){
         console.log("The given email is invalid");
+        return false;
     }
     else if (User_Email.charAt(User_Email.length - 4) === "." || User_Email.charAt(User_Email.length - 3 ) === "."){
-        console.log("Valid email");
+        return false;
     }
     else{
-        console.log("Invalid email! Please check??");
+        Email_Error.textContent= "Invalid email! Please check??";
     }
 
     // Password validation
 
-    if(User_Password === null){
-        if(User_Password !== User_Confirm_Password){
-
+    if(User_Password.trim() === ""){
+        Password_Error.textContent = "Please enter the password";
+    }
+    else if (User_Password.length < 8 || User_Password.length > 20){
+        Password_Error.textContent = "password must lies between 8 to 20 charector";
+    }
+    else if (User_Password !== User_Confirm_Password){
+        Confirm_Password_Error.textContent = "Password must be same.";
+    }
+    else{
+        const hasUpperCase = /[A-Z]/.test(User_Password);
+        const hasLowerCase = /[a-z]/.test(User_Password);
+        const hasNumber = /[0-9]/.test(User_Password);
+        const hasSpecialChar = /[!@#$%^&*()|\/;:<>?]/.test(User_Password);
+        if(!hasUpperCase){
+            Password_Error.textContent = "password must include at least one uppercase letter!"
         }
-        else if(User_Password.trim() === ""){
-            console.log("Please enter the password");
+        else if(!hasLowerCase){
+            Password_Error.textContent = "password must include at least one lowercase letter!"
         }
-        else if (User_Password !== User_Confirm_Password){
-            console.log("Password must be same.");
+        else if(!hasNumber){
+            Password_Error.textContent = "password must include at least one number!"
+        }
+        else if(!hasSpecialChar){
+            Password_Error.textContent = "password must include at least one special letter!"
+        }
+        else{
+            alert("password created successfully!");
         }
     }
 
+
+    // Contact validation
+
+    if(User_Contact.length< 10 || User_Contact.length > 10){
+        Contact_Error.textContent = "Number Should be of 10 Digits!"
+    }
+    else if (isNaN(User_Contact)){
+        Contact_Error.textContent = "Only Numbers are allowed!"
+    }
 
 }
