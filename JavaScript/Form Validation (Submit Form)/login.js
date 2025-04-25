ee01b1ec5257c1d02ec5e06c677b446aeaaf7703
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-04-25 19:03:42 +0530

    Updated code

--- JavaScript/Form Validation (Submit Form)/login.js
+++ JavaScript/Form Validation (Submit Form)/login.js
@@ -1,12 +1,12 @@
 
-let strage = JSON.parse(localStorage.getItem("userData"));
+let storage = JSON.parse(localStorage.getItem("userData"));
 
 function loginValidation(e) {
     e.preventDefault()
     const User_Login_Email = document.querySelector("#loginemail").value;
     const User_Login_Pass = document.querySelector("#loginpassword").value;
     console.log(User_Login_Email, User_Login_Pass);
-    if (strage.email === User_Login_Email && strage.password === User_Login_Pass) {
+    if (storage.email === User_Login_Email && storage.password === User_Login_Pass) {
         console.log("succesfull");
     }
     else {
