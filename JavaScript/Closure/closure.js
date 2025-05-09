2123f2b00428d917bb3089accb17391f0cd3b6b2
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-05-09 13:10:19 +0530

    Code of closure

---
+++ JavaScript/Closure/closure.js
@@ -0,0 +1,17 @@
+// Function ka postmartom
+
+const justFunction = ()=>{
+    let n = "Raju";
+    return () =>{
+        return () =>{
+            return n;
+        }
+    }
+}
+
+let ans = justFunction();
+// let ans = justFunction()()();
+
+let newAns = ans()
+
+console.log(newAns());
