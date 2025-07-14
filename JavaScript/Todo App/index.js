799bc772ef1b4d20ce47216fc5ffc59079e71841
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-07-14 07:33:40 +0530

    Added functionality of checkBox, editbtn, deleteBtn, cancelbtn, confirmBtn & many more

--- JavaScript/Todo App/index.js
+++ JavaScript/Todo App/index.js
@@ -1,115 +1,3 @@
-// let arrStorage = JSON.parse(localStorage.getItem("todoData")) || [];
-
-// const inputBox = document.querySelector(".input_box");
-// const listContainer = document.querySelector(".list_container");
-
-// const addTask = ()=>{
-//     if(inputBox.value === ""){
-//         alert("You must write something!")
-//     }
-//     else{
-//         const Li = document.createElement("li");
-//         const btnsDiv = document.createElement("div");
-//         const editBtn = document.createElement("button");
-//         const completeBtn = document.createElement("button");
-//         const deleteBtn = document.createElement("button");
-//         Li.innerHTML = inputBox.value;
-
-//         editBtn.innerText = "EDIT";
-//         completeBtn.innerText = "COMPLETED";
-//         deleteBtn.innerText = "DELETED";
-
-//         btnsDiv.classList.add("btn_container");
-        
-//         editBtn.style.color = "#21b77f";
-//         completeBtn.style.color = "#db963b";
-//         deleteBtn.style.color = "#b02541";
-
-
-//         const idDiv = document.createElement("div");
-//         let data = {
-//             id : Math.random().toString(10).substring(2,11),
-//             todoInput : inputBox.value,
-//             isCompleted : false,
-//             isEdits : false,
-//         };
-//         idDiv.innerHTML = `<small>ID: ${data.id}</small>`;
-
-//         arrStorage.push(data);
-//         localStorage.setItem("todoData", JSON.stringify(arrStorage));
-
-
-//         btnsDiv.append(editBtn,completeBtn,deleteBtn);
-//         Li.append(idDiv, btnsDiv);
-//         listContainer.append(Li);
-//     }
-//     inputBox.value = "";
-// }
-// const addBtn = () => {
-//     listContainer.style.display = "flex";
-// }
-
-// // listContainer.addEventListener("click", function (e) {
-// //     if (e.target.tagName === "BUTTON") {
-// //         const li = e.target.closest("li");
-// //         const idDiv = li.querySelector("small");
-// //         const id = idDiv.innerText.split("ID: ")[1];
-        
-// //         if (e.target.innerText === "EDIT") {
-// //             editTask(li, id);
-// //         } 
-// //         else if (e.target.innerText === "COMPLETED") {
-// //             completeTask(li, id);
-// //         } 
-// //         else if (e.target.innerText === "DELETED") {
-// //             deleteTask(li, id);
-// //         }
-// //     }
-// // });
-
-// // const editTask = (li, id) => {
-// //     const newTask = prompt("Edit your task:", li.firstChild.textContent.trim());
-// //     if (newTask) {
-// //         li.firstChild.textContent = newTask + " ";
-// //         // Update localStorage
-// //         arrStorage = arrStorage.map(item => {
-// //             if (item.id === id) {
-// //                 item.todoInput = newTask;
-// //                 item.isEdits = true;
-// //             }
-// //             return item;
-// //         });
-// //         localStorage.setItem("todoData", JSON.stringify(arrStorage));
-// //     }
-// // };
-
-// // const completeTask = (li, id) => {
-// //     li.style.textDecoration = "line-through";
-// //     li.style.color = "#888";
-// //     // Update localStorage
-// //     arrStorage = arrStorage.map(item => {
-// //         if (item.id === id) {
-// //             item.isCompleted = true;
-// //         }
-// //         return item;
-// //     });
-// //     localStorage.setItem("todoData", JSON.stringify(arrStorage));
-// // };
-
-// // const deleteTask = (li, id) => {
-// //     li.remove();
-// //     // Update localStorage
-// //     arrStorage = arrStorage.filter(item => item.id !== id);
-// //     localStorage.setItem("todoData", JSON.stringify(arrStorage));
-// // };
-
-// // inputBox.addEventListener("keypress", function (e) {
-// //     if (e.key === "Enter") {
-// //         addTask();
-// //         addBtn();
-// //     }
-// // });
-
 let arrStorage = JSON.parse(localStorage.getItem("todoDatas")) || [];
 
 const myTodo = (event) => {
@@ -132,7 +20,6 @@ const myTodo = (event) => {
     localStorage.setItem("todoDatas",JSON.stringify(arrStorage));
     appenData();
     todoInput.value = "";
-    console.log(arrStorage,"this is from ls");
 
 }
 
@@ -146,6 +33,7 @@ const appenData = () => {
 
         let checkBox = document.createElement("input");
         checkBox.type = "checkbox";
+        checkBox.checked = e.isCompleted;
 
         let id = document.createElement("p");
         id.innerText = e.id;
@@ -163,8 +51,137 @@ const appenData = () => {
         deleteBtn.innerText = "Delete";
         deleteBtn.classList.add("delete_btn");
 
+        let editInput = document.createElement("input");
+        editInput.classList.add("editInput");
+        editInput.value = e.text;
 
-        div.append(checkBox,id,inputText,editBtn,deleteBtn);
+        
+        
+        // Functionality of Edit button
+        
+        editBtn.addEventListener("click", function() {
+            let editItem = arrStorage.map((el) =>{
+                if(e.id === el.id){
+                    return {...el,isEdited: !el.isEdited};
+                }
+                return el;
+            });
+            arrStorage = editItem;
+            localStorage.setItem("todoDatas",JSON.stringify(arrStorage));
+            appenData();
+        });
+
+        
+        // Functionality of Delete button
+        
+        deleteBtn.addEventListener("click", function() {
+            let deleteItem = arrStorage.filter((dl) => e.id != dl.id );
+            arrStorage = deleteItem;
+            localStorage.setItem("todoDatas", JSON.stringify(arrStorage));
+            appenData();
+        });
+        
+
+        // this is for when I click on the edit button then edit button will not display instead of edit button cancel & confirm button will display
+
+        let cancelBtn = document.createElement("button");
+        let confirmBtn = document.createElement("button");
+
+        cancelBtn.innerText = "Cancel";
+        confirmBtn.innerText = "Confirm";
+        cancelBtn.classList.add("cancelBtn");
+        confirmBtn.classList.add("confirmBtn");
+
+
+        // Functionality of Cancel button
+
+        cancelBtn.addEventListener("click", function() {
+            let cancelItem = arrStorage.map((cl) =>{
+                if (e.id === cl.id){
+                    return {
+                        ...cl,
+                        isEdited : false,
+                    };
+                }
+                return cl;
+            });
+            arrStorage = cancelItem;
+            localStorage.setItem("todoDatas", JSON.stringify(arrStorage));
+            appenData();
+        })
+
+
+        // Functionality of Confirm button
+
+        confirmBtn.addEventListener("click", function() {
+            let confirmItem = arrStorage.map((c) =>{
+                if (e.id === c.id){
+                    return {
+                        ...c,
+                        text : editInput.value,
+                        isEdited : false,
+                    }
+                }
+                return c;
+            });
+            arrStorage = confirmItem;
+            localStorage.setItem("todoDatas", JSON.stringify(arrStorage));
+            appenData();
+        })
+
+
+        // Functionality of Checkbox
+
+        checkBox.addEventListener("change", function() {
+            let checkBoxItem = arrStorage.map((ch) => {
+                if (e.id === ch.id){
+                    return {
+                        ...ch,
+                        isCompleted : checkBox.checked,
+                    }
+                }
+                return ch;
+            });
+            arrStorage = checkBoxItem;
+            localStorage.setItem("todoDatas", JSON.stringify(arrStorage));
+            appenData();
+        })
+
+        
+        if(e.isEdited){
+            editInput.style.display = "block";
+            inputText.style.display = "none";
+            editBtn.style.display = "none";
+            cancelBtn.style.display = "block";
+            deleteBtn.style.display = "none";
+            confirmBtn.style.display = "block";
+            checkBox.disabled = true;
+            
+        }
+        else{
+            editInput.style.display = "none";
+            editBtn.style.display = "block";
+            cancelBtn.style.display = "none";
+            deleteBtn.style.display = "block";
+            confirmBtn.style.display = "none";
+            checkBox.style.textDecoration = "none";
+        }
+        
+        if(e.isCompleted){
+            inputText.style.textDecoration = "line-through";
+            editBtn.style.textDecoration = "line-through";
+            deleteBtn.style.textDecoration = "line-through";
+            id.style.textDecoration = "line-through";
+            editBtn.disabled = true;
+        }
+        else{
+            inputText.style.textDecoration = "none";
+            editBtn.style.textDecoration = "none";
+            deleteBtn.style.textDecoration = "none";
+            id.style.textDecoration = "none";
+        }
+
+        div.append(checkBox,id,inputText,editInput,editBtn,cancelBtn,deleteBtn,confirmBtn);
         mainDiv.append(div);
 
 
