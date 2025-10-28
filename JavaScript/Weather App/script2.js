9ba22d9b9a1b0b625ef499c27067f41d4280cc0f
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-10-28 06:35:05 +0530

    Code of the day

--- JavaScript/Weather App/script2.js
+++ JavaScript/Weather App/script2.js
@@ -1,20 +1,46 @@
 let api_key = "a7f44a715578d31824ab69aeafc9ed80";
 
 // fetching weather data
-const fetchData = async() => {
-    let searchInput = document.querySelector("#search_input");
-    let searchInputVal = searchInput.value;
-    // let api = `https://api.openweathermap.org/data/2.5/weather?q=${searchInputVal}&appid=${api_key}&units=metric`
-    let api = `https://api.openweathermap.org/data/2.5/forecast?q=${searchInputVal}&units=metric&appid=${api_key}`;
+const fetchData = async () => {
+  let searchInput = document.querySelector("#search_input");
+  let searchInputVal = searchInput.value;
+  searchInputVal =
+    searchInputVal.charAt(0).toUpperCase() + searchInputVal.slice(1);
 
-    try {
-        let res = await fetch(api);
-        let data = await res.json();
-        console.log("🚀 ~ data:", data.list);
+  let api = `https://api.openweathermap.org/data/2.5/forecast?q=${searchInputVal}&units=metric&appid=${api_key}`;
 
-        // temp details
-        let tempDetails = document.querySelector(".temp-details");
-        tempDetails.innerHTML = `
+  let loading = document.querySelector(".loading");
+
+  try {
+    loading.style.display = "block";
+    loading.innerText = "Loading...";
+
+    let res = await fetch(api);
+    let data = await res.json();
+
+    if (data.cod !== "200") {
+      loading.innerText = "City not found!";
+      searchInput.value = "";
+      return;
+    }
+
+    loading.style.display = "none";
+
+    hourlyForcast(data);
+
+    console.log("🚀 ~ data:", data.list);
+
+    // weather details
+
+    let weatherIcon = document.querySelector(".weather-icon");
+    weatherIcon.innerHTML = `
+            <img src="https://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png" alt="weather icon">
+            <p>${data.list[0].weather[0].description}</p>
+        `;
+
+    // temp details
+    let tempDetails = document.querySelector(".temp-details");
+    tempDetails.innerHTML = `
         <p>${data.list[0].main.temp}&deg;C</p>
         <div>
             <div class="feels-like">
@@ -27,9 +53,9 @@ const fetchData = async() => {
                 <p><span>${data.list[0].main.temp_max}&deg;</span> / <span>${data.list[0].main.temp_min}&deg;</span></p>
             </div>
         </div>
-    `
+    `;
 
-      // weather stats
+    // weather stats
 
     let weatherStats = document.querySelector(".weather-stats");
     weatherStats.innerHTML = `
@@ -37,7 +63,7 @@ const fetchData = async() => {
                 <i class="bi bi-droplet"></i>
                 <div class="stat-info">
                     <p class="stat-title">Humidity</p>
-                    <h2 class="stat-value">${data.main.humidity}%</h2>
+                    <h2 class="stat-value">${data.list[0].main.humidity}%</h2>
                 </div>
             </div>
 
@@ -45,7 +71,7 @@ const fetchData = async() => {
                 <i class="bi bi-wind"></i>
                 <div class="stat-info">
                     <p class="stat-title">Wind Speed</p>
-                    <h2 class="stat-value">${data.wind.speed} km/h</h2>
+                    <h2 class="stat-value">${data.list[0].wind.speed} km/h</h2>
                 </div>
             </div>
 
@@ -61,45 +87,41 @@ const fetchData = async() => {
                 <i class="bi bi-speedometer"></i>
                 <div class="stat-info">
                     <p class="stat-title">Pressure</p>
-                    <h2 class="stat-value">${((data.main.pressure)/1013.25).toFixed(2)} atm</h2>
+                    <h2 class="stat-value">${(
+                      data.list[0].main.pressure / 1013.25
+                    ).toFixed(2)} atm</h2>
                 </div>
             </div>
 
-    `
+    `;
 
     searchInput.value = "";
-    
-    } catch (error) {
-        console.log(error)
-    }
-    finally{
+  } catch (error) {
+    console.log(error);
+    loading.innerText = "Something went wrong!";
+  } finally {
+    setTimeout(() => {
+      loading.style.display = "none";
+    }, 2000);
+  }
 
-    }
+  let weatherLocationTime = document.querySelector(".location-time");
 
-    let weatherLocationTime = document.querySelector(".location-time");
-
-    weatherLocationTime.innerHTML = `
+  weatherLocationTime.innerHTML = `
                 <div>
                     <i class="bi bi-geo-alt"></i>
                     <p>${searchInputVal}</p>
                 </div>
-                <p>${moment().format('MMMM Do YYYY, h:mm:ss a')}</p>
-    `
-
-    // weather details
-
-    let weatherIcon = document.querySelector(".weather-icon");
-    weatherIcon.innerHTML = `
-        <i class="bi bi-cloud"></i>
-        <p>Cloud Type(Sunny or Partly Cloudy)</p>
-    `
-
-  
-
-}
+                <p>${moment().format("MMMM Do YYYY, h:mm:ss a")}</p>
+    `;
+};
 
 // weather details
 
-const weatherDetails = () => {
+const hourlyForcast = (data) => {
+  console.log("🚀 ~ data:", data);
 
-}
+  // data.forEach((el)=> {
+
+  // })
+};
