975c953212af484bcbc0bdae10ac8be8914e126b
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2026-01-19 13:32:12 +0530

    Code of the day

--- JavaScript/Patterns/index.js
+++ JavaScript/Patterns/index.js
@@ -6,22 +6,113 @@
 //     console.log( row);
 // }
 
-let n = 4
+let n = 5;
 
-for(let i = 1; i <= n; i++){
-    let row = ""
-    for(let j = 1; j<= n; j++){
-        row += i + " "
-    }
-    console.log(row)
+for (let i = 1; i <= n; i++) {
+  let row = "";
+  for (let j = 1; j <= n; j++) {
+    row += i + " ";
+  }
+  console.log(row);
 }
 
-console.log("")
+console.log("");
 
-for(let i = 1; i <= n; i++){
-    let row = ""
-    for(let j = 1; j<= n; j++){
-        row += j + " "
-    }
-    console.log(row)
+for (let i = 1; i <= n; i++) {
+  let row = "";
+  for (let j = 1; j <= n; j++) {
+    row += j + " ";
+  }
+  console.log(row);
 }
+
+console.log("");
+
+// Right Triangle Patterns
+
+for (let i = 1; i <= n; i++) {
+  let row = "";
+  for (let j = 1; j <= i; j++) {
+    row += "* ";
+  }
+  console.log(row);
+}
+
+console.log("");
+
+// Print a right triangle of numbers starting from 1.
+
+for (let i = 1; i <= n; i++) {
+  let row = "";
+  for (let j = 1; j <= i; j++) {
+    row += j + " ";
+  }
+  console.log(row);
+}
+
+console.log("");
+
+// Print a right triangle where each row contains the same number.
+
+for (let i = 1; i <= n; i++) {
+  let row = "";
+  for (let j = 1; j <= i; j++) {
+    row += i + " ";
+  }
+  console.log(row);
+}
+
+console.log("");
+
+//! Reverse Patterns
+
+// Print a reverse right triangle of stars.
+
+for (let i = n; i >= 1; i--) {
+  let row = "";
+  for (let j = 1; j <= i; j++) {
+    row += "* " + "";
+  }
+  console.log(row);
+}
+
+console.log("");
+
+// Print a reverse number triangle.
+
+for (let i = n; i >= 1; i--) {
+  let row = "";
+  for (let j = 1; j <= i; j++) {
+    row += j + " ";
+  }
+  console.log(row);
+}
+
+console.log("");
+
+// Print a reverse triangle with same numbers in each row.
+
+for (let i = n; i >= 1; i--) {
+  let row = "";
+  for (let j = 1; j <= i; j++) {
+    row += i + " ";
+  }
+  console.log(row);
+}
+
+console.log("");
+
+// ! Space + Star Patterns
+
+for (let i = 1; i <= n; i++) {
+  let row = " ";
+  for(let s = 1; s<= n-i; s++){
+    row += " "
+  }
+  for (let j = 1; j <= i; j++) {
+    row += "*";
+  }
+  console.log(row);
+}
+
+// for (let i = 1 ; i <= n ; i++){}
