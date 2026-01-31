95dc89c88b4a7373c2800ad1adcca96b51475570
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2026-01-31 22:25:27 +0530

    Code of the day

--- JavaScript/Patterns/index.js
+++ JavaScript/Patterns/index.js
@@ -11,7 +11,8 @@ let n = 5;
 for (let i = 1; i <= n; i++) {
   let row = "";
   for (let j = 1; j <= n; j++) {
-    row += i + " ";
+    // row += i + " ";
+    row = row + i + " ";
   }
   console.log(row);
 }
